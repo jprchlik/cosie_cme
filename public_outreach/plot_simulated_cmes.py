73380b15ad8b1c73f6f43572f6747c7b26a4ec42
jprchlik <japrchlik@gmail.com>
2018-03-21 11:49:09 -0400

    Added edits to figure requested by Ed

--- public_outreach/plot_simulated_cmes.py
+++ public_outreach/plot_simulated_cmes.py
@@ -1,12 +1,22 @@
-import matplotlib
-matplotlib.rc('font',size=24)
+import matplotlib as mpl
+mpl.rc('font',size=24)
 import numpy as np
 from fancy_plot import *
 import pandas as pd
 import matplotlib.pyplot as plt
-from mpl_toolkits.axes_grid1.inset_locator import inset_axes, zoomed_inset_axes
+from mpl_toolkits.axes_grid1.inset_locator import inset_axes, zoomed_inset_axes,InsetPosition
 
 kmsolar = 6.96*10.**5. #solar radii to km
+
+
+#Eds requested custom color map
+ccmap = mpl.colors.ListedColormap([ 'blue','green','yellow','red'])
+#ccmap.set_over('0.25')
+#ccmap.set_under('1.0')
+
+bounds = [np.log10(0.0003),np.log10(0.003),np.log10(0.03),np.log10(0.3),np.log10(3.)]
+norm = mpl.colors.BoundaryNorm(bounds, ccmap.N)
+
 
 #create observation obscured or unobscured Use 30s COSIE cadence
 cad = '10S'
@@ -70,8 +80,20 @@ H_plt = np.log10(H/1000./obs_yrs)
 #Switch to binned data 2018/03/13 J. Prchlik
 #ax2[1,0].scatter(cmes.v,cmes.obs_dur*scl,color='black',label='Sim. CME')
 #add max and min color values
-plotc = ax2[1,0].pcolormesh(X,Y,H_plt,label=None,cmap=plt.cm.gray.reversed(),vmax=1.0,vmin=-3.)
-ax2[1,0].errorbar(ubin,bcme.obs_dur.mean()*scl,yerr=bcme.obs_dur.std()*scl,fmt='s',color='red',label='Mean')
+#plotc = ax2[1,0].pcolormesh(X,Y,H_plt,label=None,cmap=plt.cm.viridis.reversed(),vmax=1.0,vmin=-3.)
+#custom color map at Ed's request
+plotc = ax2[1,0].pcolormesh(X,Y,H_plt,label=None,cmap=ccmap,norm=norm)
+ax2[1,0].errorbar(ubin,bcme.obs_dur.mean()*scl,yerr=bcme.obs_dur.std()*scl,fmt='s',color='black',label='Mean')
+
+#Add horizontal 90 minute orbit and 500s
+ax2[1,0].plot([-100,10000],[3600.,3600.],'--b',linewidth=3)
+ax2[1,0].text(1000,3690,'3600s (1 ISS Orbit)',fontsize=18)
+ax2[1,0].plot([-100,10000],[500.,500.],'--b',linewidth=3)
+ax2[1,0].text(2150,550,'500s',fontsize=18)
+#reset xlimit
+ax2[1,0].set_xlim([0,2400])
+
+
 ax2[1,0].set_xlabel('CACTus Velocity [km/s]')
 ax2[1,0].set_ylabel('COSIE Obs. Duration [s]')
 ax2[1,0].legend(loc='upper right',scatterpoints=1,frameon=False,handletextpad=-0.12)
@@ -93,6 +115,19 @@ ax2[0,0].set_ylim([0.,100.])
 #plot observation time histogram
 ax2[1,1].plot(100.-hplt1,hbin1,color='black',linewidth=3)
 ax2[1,1].set_yticklabels([])
+
+#Add annotation of cumlative distribution
+ax2[1,1].annotate('92% \n of CMEs\n observed', xy=(92, 1),  xycoords='data',
+            xytext=(45,1499), textcoords='data',
+            arrowprops=dict(facecolor='blue', shrink=0.0),
+            horizontalalignment='right', verticalalignment='top',
+            fontsize=18)
+ax2[1,1].annotate('50% of CMEs\n observed $>$ 1800s', xy=(50, 1800),  xycoords='data',
+            xytext=(55,6000), textcoords='data',
+            arrowprops=dict(facecolor='blue', shrink=0.0),
+            horizontalalignment='center', verticalalignment='top',
+            fontsize=18)
+
 ax2[1,1].set_xlabel('Cumulative COSIE \n Obs. Duration [%]')
 ax2[1,1].set_ylim(ax2[1,0].get_ylim())
 ax2[1,1].set_xlim([0.,105.])
@@ -102,10 +137,15 @@ ax2[0,1].axis('off')
 #turn top right figure into color bar
 #cbar = fig2.colorbar(plotc,cax=ax2[0,1])
 #use inset axis instead
+
+#switch to axis locator 2018/03/21 J. Prchlik
 axins = inset_axes(ax2[1,0],
                    width="5%",  # width = 30% of parent_bbox
                    height="40%",  # height : 1 inch
-                   loc=7,borderpad=4.0)
+                   loc=9,borderpad=1.0)
+
+
+
 cbar = fig2.colorbar(plotc,cax=axins)
 cbar.set_label('Log(N$_\mathrm{CMEs}$/year)',fontsize=18)
 cbar.set_ticks([-3.0,-2.0,-1.0,0.,1.0])
